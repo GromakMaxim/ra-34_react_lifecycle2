3fc742e3b6b283f65cd8ae39c632792efb465e8e
MaximGromak <maximgromak@gmail.com>
2022-04-12 20:40:26 +1000

    clean up code

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Widget from "./components/Widget";
 import React from "react";
 
 function App() {
-  return (
-    <Widget/>
-  );
+    return (
+        <Widget/>
+    );
 }
 
 export default App;
